971ef5025dfe256a1f09a0b8dd45e58a62ce3988
Bisu56 <rajbanshibishal441@gmail.com>
2026-02-12 22:12:09 +0545

    ui of car list

--- car-listing.php
+++ car-listing.php
@@ -69,162 +69,154 @@ ini_set('display_errors', 1);
 <section class="listing-page">
   <div class="container">
     <div class="row">
-      <div class="col-md-9 col-md-push-3">
+      <div class="col-md-9">
         <div class="result-sorting-wrapper">
           <div class="sorting-count">
 <?php 
-// Query for Listing count
-$sql = "SELECT id FROM tblvehicles";
-$query = $dbh->prepare($sql);
+//Query for Listing count
+$sql = "SELECT id from tblvehicles";
+$query = $dbh -> prepare($sql);
 $query->execute();
-$vehicleCount = $query->rowCount();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=$query->rowCount();
 ?>
-<p><span><?php echo htmlentities($vehicleCount); ?> Listings</span></p>
+<p><span><?php echo htmlentities($cnt);?> Listings</span></p>
 </div>
 </div>
 
-<?php 
-// Main query to fetch car listings
-$sql = "SELECT tblvehicles.*, tblbrands.BrandName, tblbrands.id as bid  
-        FROM tblvehicles 
-        JOIN tblbrands ON tblbrands.id = tblvehicles.VehiclesBrand";
-$query = $dbh->prepare($sql);
+<?php $sql = "SELECT tblvehicles.*,tblbrands.BrandName,tblbrands.id as bid  from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand";
+$query = $dbh -> prepare($sql);
 $query->execute();
-$results = $query->fetchAll(PDO::FETCH_OBJ);
-
-// Debugging: Print results
-
-if ($query->rowCount() > 0) {
-    foreach ($results as $result) {
-?>
-  <div class="product-listing-m gray-bg">
-    <div class="product-listing-img">
-      <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
-        <img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>" class="img-responsive" alt="Image" />
-      </a>
-    </div>
-    <div class="product-listing-content">
-      <h5>
-        <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
-          <?php echo htmlentities($result->BrandName); ?> , <?php echo htmlentities($result->VehiclesTitle); ?>
-        </a>
-      </h5>
-      <p class="list-price">
-        Nrs<?php echo !empty($result->dynamic_price) ? htmlentities($result->dynamic_price) : htmlentities($result->PricePerDay); ?> Per Day
-      </p>
-      <ul>
-        <li><i class="fa fa-user" aria-hidden="true"></i> <?php echo htmlentities($result->SeatingCapacity); ?> seats</li>
-        <li><i class="fa fa-calendar" aria-hidden="true"></i> <?php echo htmlentities($result->ModelYear); ?> model</li>
-        <li><i class="fa fa-car" aria-hidden="true"></i> <?php echo htmlentities($result->FuelType); ?></li>
-      </ul>
-      <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>" class="btn">
-        View Details <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
-      </a>
-    </div>
-  </div>
-<?php 
-    }
-} else {
-    echo "<p>No vehicles found.</p>";
-}
-?>
-
-<!-- Sidebar -->
-<aside class="col-md-3 col-md-pull-9">
-  <div class="sidebar_widget">
-    <div class="widget_heading">
-      <h5><i class="fa fa-filter" aria-hidden="true"></i> Find Your Car </h5>
-    </div>
-    <div class="sidebar_filter">
-      <form action="search-carresult.php" method="post">
-        <div class="form-group select">
-          <select class="form-control" name="brand">
-            <option>Select Brand</option>
-            <?php 
-            $sql = "SELECT * FROM tblbrands";
-            $query = $dbh->prepare($sql);
-            $query->execute();
-            $results = $query->fetchAll(PDO::FETCH_OBJ);
-            if ($query->rowCount() > 0) {
-                foreach ($results as $result) {
-                    echo "<option value='".htmlentities($result->id)."'>".htmlentities($result->BrandName)."</option>";
-                }
-            }
-            ?>
-          </select>
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{  ?>
+        <div class="product-listing-m wow fadeInUp">
+          <div class="product-listing-img"><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" class="img-responsive" alt="Image" /> </a> 
+          </div>
+          <div class="product-listing-content">
+            <h5><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a></h5>
+            <p class="list-price">Nrs<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
+            <ul class="list-style-none">
+              <li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity);?> seats</li>
+              <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear);?> model</li>
+              <li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($result->FuelType);?></li>
+            </ul>
+            <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>" class="btn">View Details <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a>
+          </div>
         </div>
-        <div class="form-group select">
-          <select class="form-control" name="fueltype">
-            <option>Select Fuel Type</option>
-            <option value="Petrol">Petrol</option>
-            <option value="Diesel">Diesel</option>
-            <option value="CNG">CNG</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <button type="submit" class="btn btn-block"><i class="fa fa-search" aria-hidden="true"></i> Search Car</button>
-        </div>
-      </form>
-    </div>
-  </div>
+      <?php }} ?>
+         </div>
+      
+      <!--Side-Bar-->
+      <aside class="col-md-3">
+        <div class="sidebar_widget">
+          <div class="widget_heading">
+            <h5><i class="fa fa-filter" aria-hidden="true"></i> Find Your Car </h5>
+          </div>
+          <div class="sidebar_filter">
+            <form action="search-carresult.php" method="post">
+              <div class="form-group select">
+                <select class="form-control" name="brand">
+                  <option>Select Brand</option>
 
-  <div class="sidebar_widget">
-    <div class="widget_heading">
-      <h5><i class="fa fa-car" aria-hidden="true"></i> Recently Listed Cars</h5>
-    </div>
-    <div class="recent_addedcars">
-      <ul>
-<?php 
-$sql = "SELECT tblvehicles.*, tblbrands.BrandName, tblbrands.id as bid 
-        FROM tblvehicles 
-        JOIN tblbrands ON tblbrands.id = tblvehicles.VehiclesBrand 
-        ORDER BY id DESC 
-        LIMIT 4";
-$query = $dbh->prepare($sql);
+                  <?php $sql = "SELECT * from  tblbrands ";
+$query = $dbh -> prepare($sql);
 $query->execute();
-$results = $query->fetchAll(PDO::FETCH_OBJ);
-if ($query->rowCount() > 0) {
-    foreach ($results as $result) {
-?>
-        <li class="gray-bg">
-          <div class="recent_post_img"> 
-            <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
-              <img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>" alt="image">
-            </a> 
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{       ?>  
+<option value="<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?></option>
+<?php }} ?>
+                 
+                </select>
+              </div>
+              <div class="form-group select">
+                <select class="form-control" name="fueltype">
+                  <option>Select Fuel Type</option>
+<option value="Petrol">Petrol</option>
+<option value="Diesel">Diesel</option>
+<option value="CNG">CNG</option>
+                </select>
+              </div>
+             
+              <div class="form-group">
+                <button type="submit" class="btn btn-block"><i class="fa fa-search" aria-hidden="true"></i> Search Car</button>
+              </div>
+            </form>
           </div>
-          <div class="recent_post_title"> 
-            <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
-              <?php echo htmlentities($result->BrandName); ?> , <?php echo htmlentities($result->VehiclesTitle); ?>
-            </a>
-            <p class="widget_price">Nrs<?php echo htmlentities($result->PricePerDay); ?> Per Day</p>
-          </div>
-        </li>
-<?php 
-    }
-}
-?>
-      </ul>
-    </div>
-  </div>
-</aside>
-<!-- /Sidebar -->
+        </div>
 
+        <div class="sidebar_widget">
+          <div class="widget_heading">
+            <h5><i class="fa fa-car" aria-hidden="true"></i> Recently Listed Cars</h5>
+          </div>
+          <div class="recent_addedcars">
+            <ul>
+<?php $sql = "SELECT tblvehicles.*,tblbrands.BrandName,tblbrands.id as bid  from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand order by id desc limit 4";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{  ?>
+
+              <li class="gray-bg">
+                <div class="recent_post_img"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" alt="image"></a> </div>
+                <div class="recent_post_title"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a>
+                  <p class="widget_price">Nrs<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
+                </div>
+              </li>
+              <?php }} ?>
+              
+            </ul>
+          </div>
+        </div>
+      </aside>
+      <!--/Side-Bar--> 
     </div>
   </div>
 </section>
+<!-- /Listing--> 
 
-<?php include('includes/footer.php'); ?>
-<?php include('includes/login.php'); ?>
-<?php include('includes/registration.php'); ?>
-<?php include('includes/forgotpassword.php'); ?>
+<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+<?php include('includes/forgotpassword.php');?>
 
 <!-- Scripts --> 
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script> 
 <script src="assets/js/interface.js"></script> 
+<!--Switcher-->
 <script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
 <script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
 <script src="assets/js/slick.min.js"></script> 
 <script src="assets/js/owl.carousel.min.js"></script>
+
 </body>
 </html>
